7167a484aa0b1077d28d8593d869e52057f9d0f1
Harbansyadav <Harbansyadav100@gmail.com>
2021-11-25 23:32:39 +0530

    Project

---
+++ privacy.php
@@ -0,0 +1,197 @@
+<?php include_once("includes/basic_includes.php");?>
+<?php include_once("functions.php"); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Find Your Perfect Partner - Makemylove
+ | Privacy :: Make My Love
+</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+<!----font-Awesome----->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!----font-Awesome----->
+<script>
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');       
+        }
+    );
+});
+</script>
+</head>
+<body>
+<!-- ============================  Navigation Start =========================== -->
+<?php include_once("includes/navigation.php");?>
+<!-- ============================  Navigation End ============================ -->
+<div class="grid_3">
+  <div class="container">
+   <div class="breadcrumb1">
+     <ul>
+        <a href="index.php"><i class="fa fa-home home_1"></i></a>
+        <span class="divider">&nbsp;|&nbsp;</span>
+        <li class="current-page">Privacy Policy</li>
+     </ul>
+   </div>
+   <div class="terms_1">
+	    <h3>Our Commitment to Privacy</h3>	
+	    <ul class="feature_list">
+			<p>
+			Our Privacy Policy was developed as an extension of our commitment to combine the 
+			highest-quality products and services with the highest level of integrity in dealing with our clients and partners. 
+			The Policy is designed to assist you in understanding how we collect, use and safeguard the personal information 
+			you provide to us and to assist you in making informed decisions when using. our site and our products and services. 
+			This statement will be continuously assessed against new technologies, 
+			business practices and our customers' needs.
+		</ul>
+	    <h5>What Information Do We Collect? </h5>   
+	    <ul class="feature_list feature_list1"><p>
+		When you visit our Web site you may provide us with two types of information:<br>
+			1) Personal information you knowingly choose to disclose that is collected on an individual basis.<br>
+			2) Web site use information collected on an aggregate basis as you and others browse our Web site
+		</ul>
+
+		<h5>How Do We Use the Information That You Provide to Us? </h5>   
+	    <ul class="feature_list feature_list1"><p>
+		Broadly speaking, we use personal information for purposes of administering our business activities, 
+		providing customer service and making available other products and services to our customers and prospective customers.
+		<br><br>
+		Occasionally, we may also use the information we collect to notify you about important changes to our Web site, 
+		new services and special offers we think you will find valuable. The lists used to send you product and service offers are 
+		developed and managed under our traditional corporate standards designed to safeguard the security and privacy of our customers’ 
+		personal information.<br><br>
+		As a customer, you will be given the opportunity, through our website, to notify us of your desire not to receive these offers.
+ 		</ul>
+
+		 <h3>What Are Cookies?</h3>	
+	    <ul class="feature_list">
+			<p>
+			Cookies are a feature of Web browser software that allows Web servers to recognize the computer used to access a Web site. 
+			Cookies are small pieces of data that are stored by a user’s Web browser on the user’s hard drive. 
+			Cookies can remember what information a user accesses on one Web page to simplify subsequent interactions with that Web site by 
+			the same user or to use the information to streamline the user’s transactions on related Web pages. 
+			This makes it easier for a user to move from Web page to Web page and to complete commercial transactions over the Internet. 
+			Cookies should make your online experience easier and more personalized.
+		</ul>
+		<br>
+
+		<h3>How Do We Use Information We Collect from Cookies?</h3>	
+	    <ul class="feature_list">
+			<p>
+			We use Web site browser software tools such as cookies and Web server logs to gather information about our Web site users’ browsing 
+			activities, in order to constantly improve our Web site and better serve our customers. 
+			This information assists us to design and arrange our Web pages in the most user- friendly manner and to continually improve 
+			our Web site to better meet the needs of our customers and prospective customers. 
+			Cookies help us collect important business and technical statistics. 
+			The information in the cookies lets us trace the paths followed by users to our Web site as they move from one page to another. 
+			Web server logs allow us to count how many people visit our Web site and evaluate our Web site’s visitor capacity. 
+			We do not use these technologies to capture your individual email address or any personally identifying information about you although 
+			they do permit us to send focused online banner advertisements or other such responses to you.
+		</ul>
+		<br>
+		<h3>Sharing Information with Affiliates</h3>	
+	    <ul class="feature_list">
+			<p>
+			From time to time you may notice offers from outside companies advertised on our Web site. We take measures to select 
+			product or service providers that are responsible and afford privacy protections to their customers. 
+			However, we cannot make any representations about the practices and policies of these companies.
+		</ul>
+		<br>
+
+		<h3>Sharing Information with Strategic Partners</h3>	
+	    <ul class="feature_list">
+			<p>
+			We may enter into strategic marketing alliances or partnerships with third parties who may be given access to personal 
+			information including your name, address, telephone number and email for the purpose of providing you information regarding products 
+			and services that we think will be of interest to you.In connection with strategic marketing alliances or partnerships, we will 
+			retain all ownership rights to the information, and we will not share information regarding your social security number or other personal financial data.
+		</ul>
+		<br>
+
+		<h3>Notice Of New Service And Changes</h3>	
+	    <ul class="feature_list">
+			<p>
+			Occasionally, we may also use the information we collect to notify you about important changes to our Web site, new services and special offers 
+			we think you will find valuable. As our client, you will be given the opportunity to notify us of your desire not to receive these 
+			offers by clicking on a response box when you receive such an offer or by opening a support ticket at members support
+		</ul>
+		<br>
+
+		<h3>How Do We Secure Information Transmissions?</h3>	
+	    <ul class="feature_list">
+			<p>
+			We use Evalon Inc as our payment gateway. 
+			We do not collect any credit card information other than that provided to us through Evalon Inc letting us know who successfully used their credit 
+			card on our site. Other email that you may send to us may not be secure unless we advise you that security measures will be in place prior to your 
+			transmitting the information. For that reason,we ask that you do not send confidential information such as Social Security or account numbers to us 
+			through an unsecured email.
+ 
+		</ul>
+		<br>
+		<h3>How Do We Protect Your Information?</h3>	
+	    <ul class="feature_list">
+			<p>
+			Information Security. We utilize encryption/security software to safeguard the confidentiality of personal information we collect from unauthorized 
+			access or disclosure and accidental loss, alteration or destruction. Evaluation of Information Protection Practices. Periodically, our operations and 
+			business practices are reviewed for compliance with corporate policies and procedures governing the security, confidentiality and quality of our information. 
+			Employee Access, Training and Expectations. Our corporate values, ethical standards, policies and practices are committed to the protection of customer information. 
+			In general, our business practices limit employee access to confidential information, and limit the use and disclosure of such information to authorized persons, 
+			processes and transactions.
+		</ul>
+		<br>
+		<h3>How Can You Access and Correct Your Information?</h3>	
+	    <ul class="feature_list">
+			<p>
+			You may request access to all your personally identifiable information that we collect online and maintain in our database by opening a support ticket at members support.
+ 
+		</ul>
+		<br>
+
+		<h3>Do We Disclose Information to Outside Parties?</h3>	
+	    <ul class="feature_list">
+			<p>
+			We may provide aggregate information about our customers, sales, Web site traffic patterns and related Web site information to our affiliates or reputable third parties, 
+			but this information will not include personally identifying data, except as otherwise provided in this Privacy Policy.
+ 
+		</ul>
+		<br>
+
+		<h3>What About Legally Compelled Disclosure of Information?</h3>	
+	    <ul class="feature_list">
+			<p>
+			We may disclose information when legally compelled to do so, in other words, when we, in good faith, believe that the law requires it or for the protection of our legal rights.
+ 
+		</ul>
+		<br>
+
+		<h3>What About Other Web Sites Linked to Our Web Site?</h3>	
+	    <ul class="feature_list">
+			<p>
+			We are not responsible for the practices employed by Web sites linked to or from our Web site nor the information or content contained therein. Often links to other Web sites 
+			are provided solely as pointers to information on topics that may be useful to the users of our Web site.
+ 
+		</ul>
+				
+     </div>
+   </div>
+</div>
+
+
+<?php include_once("footer.php");?>
